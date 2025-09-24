50f9ca8eeef81444ad67db634a486a32a3d0511a
kary5678 <60301055+kary5678@users.noreply.github.com>
2025-09-24 16:36:45 -0500

    Refactor homepage to Index

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
 import LifeUpdates from "./pages/LifeUpdates";
 import NotFound from "./pages/NotFound";
 import SignUp from "./pages/SignUp";
@@ -12,7 +13,6 @@ import Summary from "./pages/Summary";
 import Home from "./pages/Home";
 import Chats from "./pages/Chats";
 import Settings from "./pages/Settings";
-import Root from "./pages/Root";
 
 const queryClient = new QueryClient();
 
@@ -25,7 +25,7 @@ const App = () => (
           <Navbar />
           <div className="pt-20">
             <Routes>
-              <Route path="/" element={<Root />} />
+              <Route path="/" element={<Index />} />
               <Route path="/life-updates" element={<LifeUpdates />} />
               <Route path="/sign-up" element={<SignUp />} />
               <Route path="/sign-in" element={<SignIn />} />
